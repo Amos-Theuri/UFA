0b430d270ef87c49c6ca8058f4b3ca187a3bb60b
Amos Theuri <amostheuri.dev@gmail.com>
2026-09-09 14:36:56 +0300

    modify slogan

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -26,7 +26,7 @@ export default function Home() {
               <span>Empowering Kenya's Next Generation</span>
             </div>
             <h1>
-              <span className="gradient-text">Together For A</span> Brighter
+              <span className="gradient-text">Together For A </span> Brighter
               Future
             </h1>
             <p>
